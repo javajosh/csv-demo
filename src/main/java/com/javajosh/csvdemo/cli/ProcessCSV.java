de24e2da4c7d8dc6d8812c8262fba5864cd59a6a
Josh Rehman <josh@joshrehman.com>
2020-05-22 20:58:59 -0400

    First attempt to do the processing in a "low level" style, without any modelling of the data. It's pretty ugly and it doesn't fully work. I think I'll redo it with a Multimap implementation tomorrow.

--- src/main/java/com/javajosh/csvdemo/cli/ProcessCSV.java
+++ src/main/java/com/javajosh/csvdemo/cli/ProcessCSV.java
@@ -11,8 +11,7 @@ import net.sourceforge.argparse4j.inf.Subparser;
 
 import java.io.File;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.*;
 
 public class ProcessCSV extends Command {
     public ProcessCSV() {
@@ -34,6 +33,8 @@ public class ProcessCSV extends Command {
                 .help("The directory into which we write the output files.");
     }
 
+    enum FIELDS {userId, lastName, firstName, version, company};
+
     @Override
     public void run(Bootstrap<?> bootstrap, Namespace namespace) throws Exception {
         String inputFileName = namespace.getString("file");
@@ -45,8 +46,60 @@ public class ProcessCSV extends Command {
         CsvReader csvReader = new CsvReader();
         csvReader.setContainsHeader(true);
         CsvContainer csv = csvReader.read(inputFile, StandardCharsets.UTF_8);
+
+        // provider to unique ids.
+        // Ugh this is ugly. TODO: use guava's multimap instead
+        Map<String, Map<String, CsvRow>> outputFiles = new HashMap();
+        Map<String, CsvRow> partition;
+        CsvRow foundRow;
         for (CsvRow row : csv.getRows()) {
-            System.out.println("First column of line: " + row.getField(0));
+            String id = row.getField(0).trim();
+//          String lastName = row.getField(1).trim();
+//          String firstName = row.getField(2).trim();
+            int version = Integer.parseInt(row.getField(3).trim());
+            String company = row.getField(4).trim();
+
+            partition = outputFiles.get(company);
+            if (partition == null){
+                partition = new HashMap();
+                partition.put(company, row);
+                continue;
+            }
+
+            foundRow = partition.get(id);
+            if (foundRow == null){
+                partition.put(id, row);
+            } else {
+                int foundVersion = Integer.parseInt(foundRow.getField(3).trim());
+                if (version > foundVersion){
+                    partition.put(id, row);
+                }
+            }
+        }
+        System.out.println(outputFiles);
+
+        //sort by name then version
+//        List<CsvRow> allRows = csv.getRows();
+//        allRows.sort((r1, r2) -> {
+//            String name1 = r1.getField(1) + r1.getField(2);
+//            String name2 = r2.getField(1) + r2.getField(2);
+//            int v1 = Integer.parseInt(r1.getField(3));
+//            int v2 = Integer.parseInt(r2.getField(3));
+//            int nameComparison = name1.compareTo(name2);
+//            return nameComparison == 0 ? v1 - v2 : nameComparison;
+//        });
+//
+//        // prune
+//        int lastId = Integer.MIN_VALUE;
+//        for (CsvRow row : csv.getRows()) {
+//            System.out.println("First column of line: " + row.getField(0));
+//        }
+
+
+
+        for (CsvRow row : csv.getRows()) {
+
+            /// System.out.println("First column of line: " + row.getField(0));
         }
 
         // Write it out
@@ -57,4 +110,7 @@ public class ProcessCSV extends Command {
         data.add(new String[] { "value1", "value2" });
         csvWriter.write(outputFile, StandardCharsets.UTF_8, data);
     }
+
+
+
 }
